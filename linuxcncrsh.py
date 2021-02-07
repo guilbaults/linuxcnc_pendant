e9f2183337bf5fb1876890740f67446a7b838f56
Simon Guilbault <simon@simonguilbault.ca>
2021-02-07 11:14:59 -0500

    Adding the connection to LinuxCNC

---
+++ linuxcncrsh.py
@@ -0,0 +1,78 @@
+import usocket
+import ure
+
+class Linuxcncrsh():
+    def __init__(self, host, port):
+        addr_info = usocket.getaddrinfo(host, port)
+        addr = addr_info[0][-1]
+        self.s = usocket.socket()
+        self.s.connect(addr)
+
+    def login(self, clientname, password, enable):
+        self.s.write("hello {} {} 1.1\r\n".format(password, clientname))
+        if self.readline() != "HELLO ACK EMCNETSVR 1.1":
+            raise RuntimeError("Wrong password")
+        self.s.write("set enable {}\r\n".format(enable))
+        self.readline()
+        self.s.write("set echo off\r\n")
+        self.readline()
+        self.s.write("set verbose on\r\n")
+        if self.readline() != "SET VERBOSE ACK":
+            raise RuntimeError("Verbose failed")
+
+    def readline(self):
+        line = self.s.readline().decode("utf-8") 
+        return line.strip()
+
+    def valid(self):
+        line = self.readline()
+        if "NACK" in line:
+            return False
+        else:
+            return True
+
+    def get_onoff(self, param):
+        self.s.write("get {}\r\n".format(param))
+        line = self.readline()
+        if "ON" in line:
+            return True
+        else:
+            return False
+
+    def set_onoff(self, param, value):
+        if value is True:
+            self.s.write("set {} ON\r\n".format(param))
+        else:
+            self.s.write("set {} OFF\r\n".format(param))
+        return self.valid()
+
+    def get_pos(self, pos_type):
+        self.s.write("get {}\r\n".format(pos_type))
+        pos = self.readline().split()
+        return (float(pos[1]), float(pos[2]), float(pos[3]))
+
+if __name__ == "__main__":
+    import time
+    config = dict()
+    with open("config", "r") as f:
+        for line in f:
+            config_line = line.split("=")
+            key = config_line[0].strip()
+            value = config_line[1].strip()
+            config[key] = value
+
+    cnc = Linuxcncrsh(config["host"], config["port"])
+
+    cnc.login(config["login"], config["password"], config["enable"])
+
+    print("get estop", cnc.get_onoff("estop"))
+    cnc.set_onoff("estop", True)
+    time.sleep(0.1)
+    print("get estop", cnc.get_onoff("estop"))
+    cnc.set_onoff("estop", False)
+    time.sleep(0.1)
+    print("get estop", cnc.get_onoff("estop"))
+
+    cnc.set_onoff("machine", True)
+
+    print(cnc.get_pos("rel_act_pos"))
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import machine
 import display
 import network
+import utime
+from linuxcncrsh import Linuxcncrsh
+
+config = dict()
+with open("config", "r") as f:
+    for line in f:
+        config_line = line.split("=")
+        key = config_line[0].strip()
+        value = config_line[1].strip()
+        config[key] = value
 
 wifi=network.WLAN(network.STA_IF)
 wifi.active(True)
 
-wifi.connect("changeme","changeme")
+wifi.connect(config["wifi_ap"], config["wifi_password"])
 
-#utime.sleep_ms(3000)
-#network.telnet.start(user="changeme",password="changeme")
+utime.sleep_ms(3000)
+
+cnc = Linuxcncrsh(config["host"], config["port"])
+cnc.login(config["login"], config["password"], config["enable"])
 
 tft = display.TFT()
 tft.init(tft.ST7789, bgr=False, miso=17, backl_pin=4,
@@ -18,12 +30,6 @@ tft.setwin(53, 40, 53+134, 40+239)
 tft.set_bg(0xFFFFFF - tft.BLACK)
 
 tft.clear()
-tft.font(tft.FONT_DejaVu18)
-
-i = -9999.99
-tft.text(0,0, "X: {:9.2f}".format(i), 0xFFFFFF - tft.WHITE)
-tft.text(0,20, "Y: {:9.2f}".format(i), 0xFFFFFF - tft.WHITE)
-tft.text(0,40, "Z: {:9.2f}".format(i), 0xFFFFFF - tft.WHITE)
 
 tft.line(0, 60, 134, 60, 0xFFFFFF - tft.WHITE)
 
@@ -38,6 +44,14 @@ tft.text(tft.CENTER, 190, "Battery:{:.0f}%".format(95), 0xFFFFFF - tft.WHITE)
 tft.rect(0, 210, 134, 40, 0xFFFFFF - tft.RED, 0xFFFFFF - tft.RED)
 tft.font(tft.FONT_DejaVu18, transparent=True)
 tft.text(tft.CENTER, 220, "ESTOP", 0xFFFFFF - tft.WHITE)
+
+while True:
+    tft.font(tft.FONT_DejaVu18, transparent=False)
+    pos = cnc.get_pos("rel_act_pos")
+    tft.text(0,0, "X: {:9.2f}".format(pos[0]), 0xFFFFFF - tft.WHITE)
+    tft.text(0,20, "Y: {:9.2f}".format(pos[1]), 0xFFFFFF - tft.WHITE)
+    tft.text(0,40, "Z: {:9.2f}".format(pos[2]), 0xFFFFFF - tft.WHITE)
+
 
 #tft.line(0, 0, 0, 239)
 #tft.line(0, 0, 134, 0)
