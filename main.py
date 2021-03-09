a6076f4bdee95c7ee0f07244aba3afcdf0911fab
Simon Guilbault <simon@simonguilbault.ca>
2021-03-08 21:27:42 -0500

    Configuring the encoder (#1)

--- main.py
+++ main.py
@@ -1,69 +1,50 @@
 import machine
-import display
+import st7789 as display
 import network
 import utime
-from linuxcncrsh import Linuxcncrsh
 import mfd
+from linuxcncrsh import Linuxcncrsh
 import micropython
+import vga1_8x16 as font
+
 micropython.alloc_emergency_exception_buf(100)
 
-class Encoder:
-    def __init__(self, pinA, pinB):
-        self.position = 0
-        self.state = 0
-        self.pinA = machine.Pin(pinA, machine.Pin.IN, handler=self.callback, trigger=machine.Pin.IRQ_ANYEDGE, debounce=100, acttime=100)
-        self.pinB = machine.Pin(pinB, machine.Pin.IN, handler=self.callback, trigger=machine.Pin.IRQ_ANYEDGE, debounce=100, acttime=100)
-        self.state_matrix = [0,-1,1,None,1,0,None,-1,-1,None,0,1,None,1,-1,0]
-        self.count = 0
+encoder = machine.DEC(
+    0,
+    machine.Pin(26, machine.Pin.IN),
+    machine.Pin(27, machine.Pin.IN))
 
-    def callback(self, pin):
-        #irq_state = machine.disable_irq()
-        new = self.pinA.irqvalue() *2 + self.pinB.irqvalue()
-        out = self.state_matrix[self.state*4 + new]
-        if out is None:
-            out = 0
-        self.state = new
-        self.position += out
-        #machine.enable_irq(irq_state)
-        self.count+=1
+class Keys:
+    def __init__(self):
+        row1 = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
+        row2 = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_UP)
+        row3 = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+        col1 = machine.Pin(25, machine.Pin.OPEN_DRAIN, value = 1)
+        col2 = machine.Pin(32, machine.Pin.OPEN_DRAIN, value = 1)
+        col3 = machine.Pin(33, machine.Pin.OPEN_DRAIN, value = 1)
 
-    def read(self):
-        return self.position
-    def count(self):
-        return self.count
+        self.keyboard_rows = [row1, row2, row3]
+        self.keyboard_cols = [col1, col2, col3]
 
-encoder = Encoder(26, 27)
+        self.scan_keys()
 
-while True:
-    print(encoder.read(), encoder.count)
-    i = 0
-    for i in range(10000):
-        i += 1
+    def scan_keys(self):
+        keys = {}
+        for col in range(3):
+            # Scan each column by setting one to gnd while floating the other ones
+            self.keyboard_cols[col].value(0)
+            for row in range(3):
+                if self.keyboard_rows[row].value() == 1:
+                    # inverted logic, 1 means button is not pressed
+                    v = False
+                else:
+                    v = True
+                keys[(col, row)] = v
+            self.keyboard_cols[col].value(1)
+        self.keys = keys
 
-row1 = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_UP)
-row2 = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_UP)
-row3 = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
-col1 = machine.Pin(25, machine.Pin.OUT_OD, value = 1)
-col2 = machine.Pin(32, machine.Pin.OUT_OD, value = 1)
-col3 = machine.Pin(33, machine.Pin.OUT_OD, value = 1)
-
-keyboard_rows = [row1, row2, row3]
-keyboard_cols = [col1, col2, col3]
-
-def scan_keys():
-    keys = {}
-    for col in range(3):
-        # Scan each column by setting one to gnd while floating the other ones
-        keyboard_cols[col].value(0)
-        for row in range(3):
-            if keyboard_rows[row].value() == 1:
-                # inverted logic, 1 means button is not pressed
-                v = False
-            else:
-                v = True
-            keys[(col, row)] = v
-        keyboard_cols[col].value(1)
-    return keys
+    def get_keys(self):
+        return self.keys
 
 config = dict()
 with open("config", "r") as f:
@@ -75,9 +56,9 @@ with open("config", "r") as f:
 
 # battery ADC is reading half the battery voltage (resistor divider)
 try:
-    adc_battery = machine.ADC(34)
-except ValueError:
+    adc_battery = machine.ADC(machine.Pin(34))
     # Ignore if the pin is already a ADC pin
+except:
     pass
 adc_battery.atten(adc_battery.ATTN_11DB) # 3.9 V
 
@@ -85,21 +66,23 @@ adc_battery.atten(adc_battery.ATTN_11DB) # 3.9 V
 ldo = machine.Pin(14, mode=machine.Pin.OUT)
 ldo.value(1)
 
-tft = display.TFT()
-tft.init(tft.ST7789, bgr=False, miso=17, backl_pin=4,
-        backl_on=1, mosi=19, clk=18, cs=5, dc=16, splash=False)
-
-tft.setwin(53, 40, 53+134, 40+239)
-tft.set_bg(0xFFFFFF - tft.BLACK)
-
-tft.clear()
+tft = display.ST7789(
+    machine.SPI(2, baudrate=30000000, polarity=1, phase=1, sck=machine.Pin(18), mosi=machine.Pin(19), miso=machine.Pin(17)),
+    135,
+    240,
+    reset=machine.Pin(23, machine.Pin.OUT),
+    cs=machine.Pin(5, machine.Pin.OUT),
+    dc=machine.Pin(16, machine.Pin.OUT),
+    backlight=machine.Pin(4, machine.Pin.OUT),
+    rotation=2)
+tft.init()
 
 wifi=network.WLAN(network.STA_IF)
 wifi.active(True)
 
 wifi.connect(config["wifi_ap"], config["wifi_password"])
 
-tft.text(tft.CENTER, 0, "Connecting", 0xFFFFFF - tft.WHITE)
+tft.text(font, "Connecting", 0, 0)
 
 while wifi.isconnected() is False:
     utime.sleep_ms(10)
@@ -107,48 +90,53 @@ try:
     cnc = Linuxcncrsh(config["host"], config["port"])
 except OSError:
     tft.clear()
-    tft.text(tft.CENTER, 0, "LinuxCNC", 0xFFFFFF - tft.WHITE)
-    tft.text(tft.CENTER, 20, "connection failed", 0xFFFFFF - tft.WHITE)
-    tft.text(tft.CENTER, 40, "Rebooting", 0xFFFFFF - tft.WHITE)
+    tft.text(font, "LinuxCNC", 0, 0)
+    tft.text(font, "connection failed", 0, 20)
+    tft.text(font, "Rebooting", 0, 40)
+
     utime.sleep(5)
     machine.reset()
 
 cnc.login(config["login"], config["password"], config["enable"])
 
-info = mfd.Info(tft, adc_battery, wifi)
-jog = mfd.Jog(tft, cnc)
-touchoff = mfd.Touchoff(tft, cnc)
-execute = mfd.Execute(tft, cnc)
+keys = Keys()
+keys.scan_keys()
+
+info = mfd.Info(tft, adc_battery, wifi, keys)
+jog = mfd.Jog(tft, cnc, keys, encoder)
+touchoff = mfd.Touchoff(tft, cnc, keys)
+execute = mfd.Execute(tft, cnc, keys)
 sleep = mfd.Sleep(tft)
 
-mfd_pages = [info, jog, touchoff, execute, sleep]
+mfd_pages = [info, jog, touchoff, execute]
 mfd_page = 0
 
-tft.clear()
+tft.fill(display.BLACK)
 mfd_pages[0].switch()
 
 while True:
-    keys = scan_keys()
+    keys.scan_keys()
     old_mfd_page = mfd_page
 
-    if keys[(0,2)]: # SW7
+    if keys.get_keys()[(2,0)]:
         # go left
         mfd_page -= 1
         mfd_page = mfd_page % len(mfd_pages)
-        while scan_keys()[(0,2)]:
+
+        while keys.get_keys()[(2,0)]:
+            keys.scan_keys()
             utime.sleep_ms(10)
 
-    if keys[(2,2)]: # SW9
+    if keys.get_keys()[(2,2)]:
         # go right
         mfd_page +=1
         mfd_page = mfd_page % len(mfd_pages)
-        while scan_keys()[(2,2)]:
+        while keys.get_keys()[(2,2)]:
+            keys.scan_keys()
             utime.sleep_ms(10)
 
     if old_mfd_page != mfd_page:
-        tft.clear()
+        tft.fill(display.BLACK)
         mfd_pages[mfd_page].switch()
 
     mfd_pages[mfd_page].render()
-    utime.sleep_us(1)
-
--- mfd.py
+++ mfd.py
@@ -1,82 +1,80 @@
-import display
+import st7789 as display
 import utime
 import machine
+import vga1_8x16 as font
 
 class Jog:
-    def __init__(self, tft, cnc):
+    def __init__(self, tft, cnc, keys, encoder):
         self.tft = tft
         self.cnc = cnc
+        self.encoder = encoder
+        self.encoder_position = 0
         self.selected_axis = 0
         self.axis_name = ["X", "Y", "Z"]
 
     def switch(self):
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(self.tft.CENTER, 0, "Jog", 0xFFFFFF - self.tft.WHITE)
-        self.tft.line(0, 22, 134, 22, 0xFFFFFF - self.tft.WHITE)
+        self.encoder.clear()
+        self.encoder_position = self.encoder.count()
+        self.tft.text(font, "Jog", 0, 0)
+        self.tft.hline(0, 20, 135, display.WHITE)
 
     def render(self):
+        delta = self.encoder.count()
+        self.encoder.clear()
+        self.encoder_position += delta
+
         pos = self.cnc.get_pos("rel_act_pos")
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(0,25, "X: {:9.2f}".format(pos[0]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(0,45, "Y: {:9.2f}".format(pos[1]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(0,65, "Z: {:9.2f}".format(pos[2]), 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "X: {:9.2f}".format(pos[0]), 0, 25)
+        self.tft.text(font, "Y: {:9.2f}".format(pos[1]), 0, 45)
+        self.tft.text(font, "Z: {:9.2f}".format(pos[2]), 0, 65)
 
-
-        self.tft.font(self.tft.FONT_Comic, transparent=False)
-        self.tft.text(self.tft.CENTER, 120, "{}".format(self.axis_name[self.selected_axis]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(self.tft.CENTER, 160, "Inc: {:.2f}".format(0.01), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(self.tft.CENTER, 180, "Speed:{}".format(1000), 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "{}".format(self.axis_name[self.selected_axis]), 0, 120)
+        self.tft.text(font, "Inc: {:.2f}".format(0.01), 0, 160)
+        self.tft.text(font, "Speed:{}".format(1000), 0, 180)
         #jog_incr {0|1|2|...} <speed> <incr>
 
+
 class Touchoff:
-    def __init__(self, tft, cnc):
+    def __init__(self, tft, cnc, keys):
         self.tft = tft
         self.cnc = cnc
         self.selected_axis = 0
         self.axis_name = ["X", "Y", "Z"]
 
     def switch(self):
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(self.tft.CENTER, 0, "Touch-off", 0xFFFFFF - self.tft.WHITE)
-        self.tft.line(0, 22, 134, 22, 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "Touch-off", 0, 0)
+        self.tft.hline(0, 20, 135, display.WHITE)
 
     def render(self):
         pos = self.cnc.get_pos("rel_act_pos")
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(0,25, "X: {:9.2f}".format(pos[0]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(0,45, "Y: {:9.2f}".format(pos[1]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(0,65, "Z: {:9.2f}".format(pos[2]), 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "X: {:9.2f}".format(pos[0]), 0, 25)
+        self.tft.text(font, "Y: {:9.2f}".format(pos[1]), 0, 45)
+        self.tft.text(font, "Z: {:9.2f}".format(pos[2]), 0, 65)
 
-        self.tft.font(self.tft.FONT_Comic, transparent=False)
-        self.tft.text(self.tft.CENTER, 120, "{}".format(self.axis_name[self.selected_axis]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(self.tft.CENTER, 160, "Offset: {:.2f}".format(6.35), 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "{}".format(self.axis_name[self.selected_axis]), 0, 120)
+        self.tft.text(font, "Offset: {:.2f}".format(6.35), 0, 160)
         # send touch off command, should look like G10 P0 L20 X0 Y0
 
 class Execute:
-    def __init__(self, tft, cnc):
+    def __init__(self, tft, cnc, keys):
         self.tft = tft
         self.cnc = cnc
         self.last_state = None
 
     def switch(self):
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(self.tft.CENTER, 0, "Execute", 0xFFFFFF - self.tft.WHITE)
-        self.tft.line(0, 22, 134, 22, 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "Execute", 0, 0)
+        self.tft.hline(0, 20, 135, display.WHITE)
 
     def render(self):
         pos = self.cnc.get_pos("rel_act_pos")
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(0,25, "X: {:9.2f}".format(pos[0]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(0,45, "Y: {:9.2f}".format(pos[1]), 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(0,65, "Z: {:9.2f}".format(pos[2]), 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "X: {:9.2f}".format(pos[0]), 0, 25)
+        self.tft.text(font, "Y: {:9.2f}".format(pos[1]), 0, 45)
+        self.tft.text(font, "Z: {:9.2f}".format(pos[2]), 0, 65)
 
         if self.cnc.get_onoff("estop") is True:
-            self.tft.font(self.tft.FONT_DejaVu18, transparent=True)
             if self.last_state != "ESTOP":
-                self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.RED, 0xFFFFFF - self.tft.RED)
-                self.tft.text(self.tft.CENTER, 210, "ESTOP", 0xFFFFFF - self.tft.WHITE)
+                #self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.RED, 0xFFFFFF - self.tft.RED)
+                self.tft.text(font, "ESTOP", 0, 210)
             self.last_state = "ESTOP"
             return
 
@@ -84,59 +82,52 @@ class Execute:
             feed = self.cnc.get("feed_override").split()[1]
             # spindle_override segfault linuxcncrsh
             #spindle = self.cnc.get("spindle_override").split()[1]
-            self.tft.text(0,100, "Feed: {:4}".format(feed), 0xFFFFFF - self.tft.WHITE)
+            self.tft.text(font, "Feed: {:4}".format(feed), 0, 100)
             #self.tft.text(0,120, "Spindle: {}".format(spindle), 0xFFFFFF - self.tft.WHITE)
 
             status = self.cnc.get("program_status").split()[1]
             if status == "RUNNING":
-                self.tft.font(self.tft.FONT_DejaVu18, transparent=True)
                 if self.last_state != "RUNNING":
-                    self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.YELLOW, 0xFFFFFF - self.tft.YELLOW)
-                    self.tft.text(self.tft.CENTER, 210, "RUNNING", 0xFFFFFF - self.tft.BLACK)
+                    #self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.YELLOW, 0xFFFFFF - self.tft.YELLOW)
+                    self.tft.text(font, "RUNNING", 0, 210)
                 self.last_state = "RUNNING"
                 return
 
             if status == "PAUSED":
-                self.tft.font(self.tft.FONT_DejaVu18, transparent=True)
                 if self.last_state != "PAUSED":
-                    self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.YELLOW, 0xFFFFFF - self.tft.YELLOW)
-                    self.tft.text(self.tft.CENTER, 210, "PAUSED", 0xFFFFFF - self.tft.BLACK)
+                    #self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.YELLOW, 0xFFFFFF - self.tft.YELLOW)
+                    self.tft.text(font, "PAUSED", 0, 210)
                 self.last_state = "PAUSED"
                 return
 
             if status == "IDLE":
-                self.tft.font(self.tft.FONT_DejaVu18, transparent=True)
                 if self.last_state != "IDLE":
-                    self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.GREEN, 0xFFFFFF - self.tft.GREEN)
-                    self.tft.text(self.tft.CENTER, 210, "IDLE", 0xFFFFFF - self.tft.BLACK)
+                    #self.tft.rect(0, 200, 134, 239, 0xFFFFFF - self.tft.GREEN, 0xFFFFFF - self.tft.GREEN)
+                    self.tft.text(font, "IDLE", 0, 210)
                 self.last_state = "IDLE"
                 return
 
 class Info:
-    def __init__(self, tft, adc, wifi):
+    def __init__(self, tft, adc, wifi, keys):
         self.tft = tft
         self.adc = adc
         self.wifi = wifi
 
     def switch(self):
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
-        self.tft.text(self.tft.CENTER, 0, "Info", 0xFFFFFF - self.tft.WHITE)
-        self.tft.line(0, 22, 134, 22, 0xFFFFFF - self.tft.WHITE)
-        ifconfig = self.wifi.ifconfig()
-        self.tft.font(self.tft.FONT_Default, transparent=False)
-        self.tft.text(self.tft.CENTER, 25, "IP addr", 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(self.tft.CENTER, 45, ifconfig[0], 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(self.tft.CENTER, 65, "Netmask", 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(self.tft.CENTER, 85, ifconfig[1], 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(self.tft.CENTER, 105, "Gateway", 0xFFFFFF - self.tft.WHITE)
-        self.tft.text(self.tft.CENTER, 125, ifconfig[2], 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "Info", 0, 0)
+        self.tft.hline(0, 20, 135, display.WHITE)
 
-        self.tft.font(self.tft.FONT_DejaVu18, transparent=False)
+        ifconfig = self.wifi.ifconfig()
+        self.tft.text(font, "IP addr", 0, 25)
+        self.tft.text(font, ifconfig[0], 0, 45)
+        self.tft.text(font, "Netmask", 0, 65)
+        self.tft.text(font, ifconfig[1], 0, 85)
+        self.tft.text(font, "Gateway", 0, 105)
+        self.tft.text(font, ifconfig[2], 0, 125)
 
     def render(self):
-        self.adc.collect(1000, len=100, wait=True)
-        volt = self.adc.collected()[3] * 2 /1000
-        self.tft.text(self.tft.CENTER, 219, "Bat: {:.2f}V".format(volt), 0xFFFFFF - self.tft.WHITE)
+        volt = self.adc.read()/4096 * 3.6 * 2
+        self.tft.text(font, "Bat: {:.2f}V".format(volt), 0, 219)
 
 class Sleep:
     """
@@ -150,7 +141,7 @@ class Sleep:
 
     def render(self):
         self.count -= 1
-        self.tft.text(self.tft.CENTER, 100, "Sleep in {}".format(self.count), 0xFFFFFF - self.tft.WHITE)
+        self.tft.text(font, "Sleep in {}".format(self.count), 0, 100)
         utime.sleep(1)
         if self.count <= 0:
             rtc = machine.RTC()
